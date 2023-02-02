db130f8ead3d656596d2bef38880315df2cb2ee4
nandanaHari <nandana.haridas23@gmail.com>
2023-02-02 17:33:33 +0530

    Created login page

---
+++ client/mydonor/src/app/user/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent {
+  model = {
+      email: '',
+      password: ''
+  };
+
+
+  onSubmit(form:any) {
+      console.log(form);
+  }
+}
