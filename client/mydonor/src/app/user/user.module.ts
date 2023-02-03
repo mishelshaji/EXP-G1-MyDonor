1423a9744b3111222935b8c140fbedc4225c1aea
Aravind <aravindkrishnan05520@gmail.com>
2023-02-03 12:31:36 +0530

    some formating

--- client/mydonor/src/app/user/user.module.ts
+++ client/mydonor/src/app/user/user.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { LandingpageComponent } from './landingpage/landingpage.component';
+import { UserLayoutComponent } from './user-layout/user-layout.component';
 
 
 @NgModule({
   declarations: [
-    LandingpageComponent
+    LandingpageComponent,
+    UserLayoutComponent
   ],
   imports: [
     CommonModule,
