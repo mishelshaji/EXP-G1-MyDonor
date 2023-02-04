f9106bfaefb4bdb05b3a96eb6f0b4722b9210c3d
Aravind <aravindkrishnan05520@gmail.com>
2023-02-04 23:36:57 +0530

    admin-navbar-base created

--- client/mydonor/src/app/admin/admin.module.ts
+++ client/mydonor/src/app/admin/admin.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { StockviewComponent } from './stockview/stockview.component';
+import { AdminNavbarComponent } from './admin-navbar/admin-navbar.component';
 
 
 @NgModule({
   declarations: [
     AdminHomeComponent,
-    StockviewComponent
+    StockviewComponent,
+    AdminNavbarComponent
   ],
   imports: [
     CommonModule,
