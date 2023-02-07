790f1559c8dfd9a584a9be9aa7c8944eb41f5e11
donamoljoychan03 <donamoljoychan03@gmail.com>
2023-02-07 10:29:37 +0530

    Admin feedback view completed

--- client/mydonor/src/app/admin/admin-routing.module.ts
+++ client/mydonor/src/app/admin/admin-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminFeedbackviewComponent } from './admin-feedbackview/admin-feedbackview.component';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
 
 const routes: Routes = [
   {
-    path: '', component: AdminHomeComponent, children: [
+    path: '', component: AdminLayoutComponent, children: [
+      { path: '', component: AdminHomeComponent },
+      { path: 'feedback', component: AdminFeedbackviewComponent }
     ]
   }
 ];
--- client/mydonor/src/app/admin/admin.module.ts
+++ client/mydonor/src/app/admin/admin.module.ts
@@ -5,13 +5,17 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
 import { StockviewComponent } from './stockview/stockview.component';
 import { AdminNavbarComponent } from './admin-navbar/admin-navbar.component';
+import { AdminFeedbackviewComponent } from './admin-feedbackview/admin-feedbackview.component';
+import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
 
 
 @NgModule({
   declarations: [
     AdminHomeComponent,
     StockviewComponent,
-    AdminNavbarComponent
+    AdminNavbarComponent,
+    AdminFeedbackviewComponent,
+    AdminLayoutComponent
   ],
   imports: [
     CommonModule,
