a73e702b270568df0145e1ea26590635c5610ce6
Aravind <aravindkrishnan05520@gmail.com>
2023-02-08 09:38:00 +0530

    manager homepage component created

--- client/mydonor/src/app/manager/manager.module.ts
+++ client/mydonor/src/app/manager/manager.module.ts
@@ -5,13 +5,17 @@ import { ManagerRoutingModule } from './manager-routing.module';
 import { ManagerHomepageComponent } from './manager-homepage/manager-homepage.component';
 import { ManagerLayoutComponent } from './manager-layout/manager-layout.component';
 import { ManagerNavbarComponent } from './manager-navbar/manager-navbar.component';
+import { RequestsComponent } from './requests/requests.component';
+import { RequestClickedComponent } from './request-clicked/request-clicked.component';
 
 
 @NgModule({
   declarations: [
     ManagerHomepageComponent,
     ManagerLayoutComponent,
-    ManagerNavbarComponent
+    ManagerNavbarComponent,
+    RequestsComponent,
+    RequestClickedComponent
   ],
   imports: [
     CommonModule,
