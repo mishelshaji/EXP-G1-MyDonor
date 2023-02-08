12e0d2a56f28a2c5614747131faf53ab98ea49f7
Aravind <aravindkrishnan05520@gmail.com>
2023-02-08 13:38:27 +0530

    errors fixed

--- client/mydonor/src/app/customer/customer-profile-edit/customer-profile-edit.component.ts
+++ client/mydonor/src/app/customer/customer-profile-edit/customer-profile-edit.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./customer-profile-edit.component.css']
 })
 export class CustomerProfileEditComponent {
-  UpdateModel = {
+  updateModel = {
     name: '',
     username: '',
     address: '',
