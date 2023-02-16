beb433c59e644e8d3d4f2164c441759f8141313f
nandanaHari <nandana.haridas23@gmail.com>
2023-02-16 14:43:58 +0530

    login-backend

--- server/MyDonor/MyDonor.WebApp/Controller/User/AccountsController.cs
+++ server/MyDonor/MyDonor.WebApp/Controller/User/AccountsController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Security.Claims;
 
 namespace MyDonor.WebApp.Controller.User
 {
@@ -25,6 +28,15 @@ namespace MyDonor.WebApp.Controller.User
                 return Ok(result);
 
             return BadRequest(result.Errors);
+        }
+
+        [Authorize(Roles = "Customer")]
+        [HttpGet("profile")]
+        public async Task<IActionResult> GetProfile()
+        {
+            var id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = await _service.GetProfileAsync(id);
+            return user == null ? NotFound() : Ok(user);
         }
     }
 }
